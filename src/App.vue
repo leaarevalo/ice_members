0d98f79b8a7b6ffe9214a44b71e10e06c66bec96
LeaArevaloSky <user_acid50@skyairline.com>
2023-08-19 10:46:51 -0300

    :sparkles: vuetify installed and implemented form

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MainView />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainView from "./MainView.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    MainView,
+  },
+};
 </script>
 
 <style>
---
+++ src/MainView.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-layout class="rounded rounded-md">
+    <v-navigation-drawer permanent class="bg-deep-purple">
+      <v-list density="compact" nav>
+        <v-list-item
+          prepend-icon="mdi-view-dashboard"
+          title="Home"
+          value="home"
+        ></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-forum"
+          title="About"
+          value="about"
+        ></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main
+      class="d-flex align-center justify-center"
+      style="min-height: 300px"
+    >
+      <v-container>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: "MainView",
+};
+</script>
