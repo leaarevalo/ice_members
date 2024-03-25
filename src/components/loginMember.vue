01ac9be2bc19bd6b9fb36bc4042a33383db587ef
AndurGaitan <andresdavidagaitan@hotmail.com>
2024-03-25 11:11:23 -0300

    modificacion form con validaciones

--- src/components/loginMember.vue
+++ src/components/loginMember.vue
@@ -1,93 +1,73 @@
 <template>
-    <div>
-      <v-img
-        class="mx-auto my-6"
-        max-width="228"
-        src="https://iglesiadecristotuc.org.ar/media/imagen/top_bar/logo.png"
-      ></v-img>
-  
-      <v-card
-        class="mx-auto pa-12 pb-8"
-        elevation="8"
-        max-width="448"
-        rounded="lg"
-      >
-        <div class="text-subtitle-1 text-medium-emphasis">Account</div>
-  
-        <v-text-field
-          v-model="email"
-          density="compact"
-          placeholder="Email"
-          prepend-inner-icon="mdi-email-outline"
-          variant="outlined"
-        ></v-text-field>
-  
-        <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
-          password
-  
-          <a
-            class="text-caption text-decoration-none text-blue"
-            href="#"
-            rel="noopener noreferrer"
-            target="_blank"
+  <v-card class="mx-auto pa-12 pb-8" elevation="8" max-width="448" rounded="lg">
+    <v-img class="mx-auto my-6" max-width="228"
+      src="https://iglesiadecristotuc.org.ar/media/imagen/top_bar/logo.png"></v-img>
+
+        <v-form @submit.prevent="LoginUser">
+          <v-text-field
+            v-model="email"
+            label="Email"
+            type="email"
+            required
+            :rules="emailRules"
+            :error-messages="emailErrors"
+          ></v-text-field>
+          
+          <v-text-field
+            v-model="password"
+            label="Password"
+            type="password"
+            required
+            :rules="passwordRules"
+            :error-messages="passwordErrors"
+          ></v-text-field>
+          
+          <v-btn
+            type="submit"
+            class="mr-4"
           >
-            Forgot login password?</a>
-        </div>
-  
-        <v-text-field
-          v-model="password"
-          :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
-          :type="visible ? 'text' : 'password'"
-          density="compact"
-          placeholder="Enter your password"
-          prepend-inner-icon="mdi-lock-outline"
-          variant="outlined"
-          @click:append-inner="visible = !visible"
-        ></v-text-field>
-  
-  
-        <v-btn
-          class="mb-8"
-          color="blue"
-          size="large"
-          variant="tonal"
-          block
-          @click="loginUser"
-        >
-          Log In
-        </v-btn>
-  
-        <v-card-text class="text-center">
-          <a
-            class="text-blue text-decoration-none"
-            href="#"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-          Regístrate ahora <v-icon icon="mdi-chevron-right"></v-icon>
-          </a>
-        </v-card-text>
+            Login
+          </v-btn>
+        </v-form>
       </v-card>
-    </div>
-  </template>
+      <v-alert
+          v-if="error"
+          type="error"
+          dismissible
+          variant="tonal"
+        >
+          {{ error }}
+      </v-alert>
+</template>
+
+<script setup>
+  import { ref } from 'vue';
+  import { login } from '../services/login.js';
+
+  const email = ref('');
+  const password = ref('');
+  const error = ref('');
   
-  <script setup>
-    import { ref } from 'vue'
-    import {login} from "../services/login.js"
+  //Validaciones email
+  const emailRules = [
+    (v) => !!v || 'Email es requerido',
+    (v) => /.+@.+\..+/.test(v) || 'Email debe ser válido'
+  ];
+  const emailErrors = ref('');
+  
+    //Validaciones password
+  const passwordRules = [
+    (v) => !!v || 'Password es requerido',
+    (v) => v.length >= 6 || 'Password debe tener al menos 6 caracteres'
+  ];
+  const passwordErrors = ref('');
 
-    const visible = ref(false)
-
-    let email = ref('')
-    let password = ref('')
-
-    const loginUser = async () => {
-      try{
-        const userData = await login(email.value, password.value)
-        console.log("Usuario autenticado", userData)
-      } catch(error){
-        console.error("Error durante el inicio de sesion", error)
-      }
+  const LoginUser = async () => {
+    try {
+      const userData = await login(email.value, password.value);
+      console.log('Usuario autenticado:', userData);
+    } catch (error) {
+      error.value = error;
     }
-    
-  </script>
-  
+  };
+</script>
